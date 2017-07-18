8152799e6a0249db1801109e01d9020fe8165096
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-07-18 17:03:49 -0400

    Remove imports of React logo and App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import UserListContainer from './UserListContainer';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello World!</p>
-          <UserListContainer />
+        <UserListContainer />
       </div>
     );
   }
